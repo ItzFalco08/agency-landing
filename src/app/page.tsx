020185fc8fa18ac757c3304b0b1826453150b244
ItzFalco08 <falcogaming2022@gmail.com>
2025-07-21 16:45:24 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="">
       <Hero/>
-      {/* <TachStack/> */}
+      <TachStack/>
       {/* <Footer/> */}
     </div>
   );
--- src/components/home/TachStack.tsx
+++ src/components/home/TachStack.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Cover } from '../ui/cover'
 
 function TachStack() {
   return (
-    <div>TachStack</div>
+    <div className='relative container '>
+      <div className='w-full h-screen relative !border-l-[1px] !border-r-[1px] !border-neutral-800 '>
+     
+
+        <div id='title' className='w-full py-8 flex justify-center'>
+          <h2 className="font-semibold text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+            Boost SEO & Performance <br /> at <Cover>light speed</Cover>
+          </h2>
+        </div>
+     
+      </div>
+    </div>
   )
 }
 
