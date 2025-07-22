6c2f60ac314b0999f1732d6c07b305734ee9731c
ItzFalco08 <falcogaming2022@gmail.com>
2025-07-22 14:35:22 +0530

    update

--- src/components/home/Bento.tsx
+++ src/components/home/Bento.tsx
@@ -7,11 +7,10 @@ function TachStack() {
     <div className='relative container '>
       <div className='w-full h-screen relative !border-l-[1px] !border-r-[1px] !border-neutral-800 '>
      
-
         <div id='title' className='w-full py-8 flex justify-center'>
-          <h3 className="font-semibold text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+          <h2 className="font-semibold text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
             Boost Sales & Conversions <br /> at <Cover>light speed</Cover>
-          </h3>
+          </h2>
         </div>
 
         {/* bento */}
