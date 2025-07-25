1724a6eed50abd4753e2d4f3dbe6726f37418cd1
ItzFalco08 <falcogaming2022@gmail.com>
2025-07-25 19:50:52 +0530

    brand updates

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { IconMenu2, IconX } from "@tabler/icons-react";
+import Image from "next/image";
 import {
   motion,
   AnimatePresence,
@@ -9,6 +10,7 @@ import {
 } from "motion/react";
 
 import React, { useRef, useState } from "react";
+import Link  from "next/link";
 
 
 interface NavbarProps {
@@ -232,18 +234,19 @@ export const MobileNavToggle = ({
 
 export const NavbarLogo = () => {
   return (
-    <a
-      href="#"
-      className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
+    <Link
+      href="/"
+      className="relative z-20 mr-4 flex items-center  px-2 py-1 text-sm font-normal text-black"
     >
-      <img
-        src="https://assets.aceternity.com/logo-dark.png"
-        alt="logo"
-        width={30}
-        height={30}
+      <Image
+        src="/images/logo.png"
+        alt="Anovas logo"
+        width={36}
+        height={36}
+        className="object-contain"
       />
-      <span className="font-medium text-black dark:text-white">Startup</span>
-    </a>
+      <span className="font-medium text-black dark:text-white">Anovas</span>
+    </Link>
   );
 };
 
